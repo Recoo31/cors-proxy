0201ada251bbaa485da21f982c61f851a3546048
Recoo <104390148+Recoo31@users.noreply.github.com>
2023-11-03 22:33:05 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -45,7 +45,7 @@ def proxy_request2():
         return jsonify({'error': 'destination parameter is required'}), 400
 
     try:
-        response = requests.get(destination_url, allow_redirects=True)
+        response = requests.get(destination_url, allow_redirects=False)
         if response.status_code == 302:
             return response.headers['Location']
         else:
