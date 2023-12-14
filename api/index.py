a809a5f05533c66a9fc9654e33442a3d8e4186cf
Recoo <104390148+Recoo31@users.noreply.github.com>
2023-12-14 17:29:25 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -36,7 +36,7 @@ def proxy_request():
             "connection": "keep-alive",
             "cookie": "uc=init=%22H4sIAAAAAAAEAIVRy27DIBD8F5%2brCAc3pr1hx24j5SHFVu4YtgoShgibVG3Vfy%2bRAolz6XFmdnd2dn%2bSJZwlh5VIXpMK59XLHNU4pyQjRUppTbJFmqOiKkmGFsnTtXrLevD1zYbu2%2fYQ6fbr5GntlApMYZkWU2pjBKgptRsOYAdptJ8Zh9XS9p%2fMQpTuO96ZFf%2bKD75Muw%2fGR2fBBmU17I0ZA2q4BdCN%2fI6d5cnRTk7RPEKjNfDR29%2fn3g3U8mNAt1x4hmeoy7LUByyVBD0%2b7H6FhZNKbF3f3bZ8A7M2nF2cLm96RgT7ULZfwzlc8vcPIXlwdsgBAAA%3d%22&uinf=0YUF6cc%2blKtJcY2N%2bYrPS5CROnstnaBgcZCz%2fhREPh7qUWTUphf5PgrL4%2bFHODlEYwpIcPjfrfn0hC5JYZYJDRXdgUcgwOF4hIhwmEY0SQZNayF7qZBVYZgCt09Z87C2VEL2wwCVbYiNRdjI0HZEtRxjddGeM33%2fpuNXepF0b3cDzCx%2fj3Kh7MNuyOXqFs3gNy6RNH52oWiF5HsqXhoag114MlGlpbNhxQAlOiVpaFhdq6fllz85q25AVNLTBSvqxvM2thrCtmMVJNM3Do6MsPMwVV4LtlAFjBPFN8tOYTAeA1IXExSb%2bQxC0V3ZjeWj; TS01f5b24d=0133743c616b8bc6a3d6365cacecb23d8e9e8d7a30194cb25c0d15184b099380ca4898e13a05c2380ca9c4548b68a09448f72c8e8b296f33b5186f19efe2a4754da7afb467"
         }
-        response = requests.get(destination_url)
+        response = requests.get(destination_url, headers=headers)
         if response.status_code == 200:
             return response.text
         else:
